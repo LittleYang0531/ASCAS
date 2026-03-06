e3ab1ec65613bbf7e0fb49b5cf1f620938471a9a
LittleYang0531 <littleyang0531@cau.edu.cn>
2026-03-06 18:19:23 +0800

    fix: Backend assertion failed when crop was not found

--- backend/api/crops/details.cpp
+++ backend/api/crops/details.cpp
@@ -4,6 +4,7 @@ auto CropsDetails = [](client_conn conn, http_request request, param argv) {
     int uid = UserUtils.checkLogin(request);
     if (!uid) quickSendCode(401);
     int cid = stoi(argv[0]);
+    if (!CropUtils.exists(cid)) quickSendCode(404);
     Crop item = CropUtils.getCropInfo(cid, uid);
     if (item.permission == UserPermission::NONE) quickSendCode(401);
     quickSendData(200, item);
