c7993dcd8a7d560fd639897c45cb0432cb1112f6
LittleYang0531 <littleyang0531@cau.edu.cn>
2026-04-06 21:20:14 +0800

    fix: infinite scroll error

--- frontend/src/components/Markdown.vue
+++ frontend/src/components/Markdown.vue
@@ -9,21 +9,22 @@ const props = defineProps<{
 }>();
 const html = ref("");
 
-async function render(text: string) {
-    const markdownit = await import('markdown-it');
-    const markdownItKatex = await import('@vscode/markdown-it-katex');
-    const hljs = await import('highlight.js');
-    const md = markdownit.default({
+import markdownit from "markdown-it";
+import markdownItKatex from "@vscode/markdown-it-katex";
+import hljs from "highlight.js";
+
+function render(text: string) {
+    const md = markdownit({
         html: true,
         breaks: true,
         highlight: function (str: string, lang: string): string {
             if (str[str.length - 1] == '\n') str = str.substr(0, str.length - 1);
-            if (lang && hljs.default.getLanguage(lang)) {
+            if (lang && hljs.getLanguage(lang)) {
                 try {
                     var code: string = '<pre class="hljs"><code><ul>';
                     var strs: string[] = str.split('\n');
                     for (var i = 0; i < strs.length; i++) 
-                        code += '<li class="hljs language-' + lang + '">' + hljs.default.highlight(lang, strs[i]!, true).value + '</li>'
+                        code += '<li class="hljs language-' + lang + '">' + hljs.highlight(lang, strs[i]!, true).value + '</li>'
                     code += '</ul></code></pre>';
                     return code;
                 } catch (__) {}
@@ -36,11 +37,11 @@ async function render(text: string) {
             return code;
         }
     });
-    md.use(markdownItKatex.default);
+    md.use(markdownItKatex);
     return md.render(text);
 }
-watchEffect(async () => {
-    html.value = await render(props.text);
+watchEffect(() => {
+    html.value = render(props.text);
 });
 </script>
 
